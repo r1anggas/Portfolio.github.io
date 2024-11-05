172ce24451c9564675e158954ff2413549da2b61
r1anggas <119504367+r1anggas@users.noreply.github.com>
2024-11-05 12:21:07 +0700

    Add files via upload

--- animasi.js
+++ animasi.js
@@ -16,4 +16,17 @@
     sections.forEach(section => {
         observer.observe(section);
     });
-    
+    // =======================================================
+    // Fungsi untuk membuka modal dan menampilkan gambar sertifikat penuh
+    function openModal(src) {
+        var modal = document.getElementById("certificateModal");
+        var fullImage = document.getElementById("fullCertificate");
+        modal.style.display = "block";
+        fullImage.src = src;
+    }
+
+    // Fungsi untuk menutup modal saat area luar gambar diklik
+    function closeModal() {
+        var modal = document.getElementById("certificateModal");
+        modal.style.display = "none";
+    }
